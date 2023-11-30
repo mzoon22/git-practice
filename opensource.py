10197f26b37718d17f9a1cca0775521a64617a6a
mzoon22 <jeealice@gachon.ac.kr>
2023-11-30 19:43:24 +0900

    work

--- opensource.py
+++ opensource.py
@@ -1,11 +1,7 @@
-total 944K
--rw-r--r-- 1 mzoon22 197121  451 Nov 30 18:34 [200~
-drwxr-xr-x 1 mzoon22 197121    0 Nov 30 18:49 ./
-drwxr-xr-x 1 mzoon22 197121    0 Nov 30 18:17 ../
-drwxr-xr-x 1 mzoon22 197121    0 Nov 30 18:49 .git/
--rw-r--r-- 1 mzoon22 197121  398 Nov 30 18:31 filename
--rw-r--r-- 1 mzoon22 197121   60 Nov 30 18:49 git
+total 940K
+drwxr-xr-x 1 mzoon22 197121    0 Nov 30 19:34 ./
+drwxr-xr-x 1 mzoon22 197121    0 Nov 30 18:51 ../
+drwxr-xr-x 1 mzoon22 197121    0 Nov 30 19:36 .git/
 -rw-r--r-- 1 mzoon22 197121 909K Nov 30 17:14 haar_face.xml
--rw-r--r-- 1 mzoon22 197121  511 Nov 30 18:34 haar_mouth.xm
 -rw-r--r-- 1 mzoon22 197121  511 Nov 30 18:36 haar_mouth.xml
--rw-r--r-- 1 mzoon22 197121    0 Nov 30 18:50 opensource.py
+-rw-r--r-- 1 mzoon22 197121    0 Nov 30 19:36 opensource.py
